c253c973fafc32c27b2dd1603ac1c5067fd3b150
mast3rwarr10r <egorm987@gmail.com>
2025-10-02 20:52:00 +0300

    правки

--- EMar.py
+++ EMar.py
@@ -12,13 +12,10 @@ df = pd.DataFrame(data)
 
 for hall in df['Зал'].unique():
     df_hall = df[df['Зал'] == hall].copy()
-    df_hall = df_hall.drop('Зал', axis=1)
+    df_hall = df_hall.drop('Зал', axis = 1)
     df_hall = df_hall.sort_values('Дата и время')
     filename = f'{hall}.xlsx'
-    with pd.ExcelWriter(filename) as writer:
-        df_hall.to_excel(writer, index=False, sheet_name = 'Расписание')
-        workbook = writer.book
-        worksheet = writer.sheets['Расписание']
-        for col_idx, value in enumerate (df_hall.columns.values):
-            worksheet.write(0, col_idx, value, workbook.add_format({'bold': True}))
-        worksheet.set_column(0, len(df_hall.columns)-1, 25)
+    with pd.ExcelWriter(filename, engine = 'xlsxwriter') as writer:
+         df_hall.to_excel(writer, index=False, sheet_name='Расписание')
+         worksheet = writer.sheets['Расписание']
+         worksheet.set_column(0, len(df_hall.columns) - 1, 25)
